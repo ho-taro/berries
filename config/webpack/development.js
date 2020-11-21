b9b089afa0e441e6e97c9a8e098e68587b8c88fa
shotaro hosoda <shotaro.hosoda.1921@gmail.com>
2020-11-21 12:34:11 +0900

    Update development.js

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,11 +3,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const environment = require('./environment')
 
 const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
 )
 
 module.exports = environment.toWebpackConfig()
