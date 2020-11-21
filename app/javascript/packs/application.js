2ac072c5cc6c53d847441e8fc4c3709697195270
shotaro hosoda <shotaro.hosoda.1921@gmail.com>
2020-11-21 13:03:41 +0900

    調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,14 +10,9 @@
 //= require activestorage
 
 require("@rails/ujs").start()
-
 require("turbolinks").start()
-
 require("@rails/activestorage").start()
-
-
 require("channels")
-
 require('jquery')
 require("../delete")
 
